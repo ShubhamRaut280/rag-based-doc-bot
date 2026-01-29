0cc2eee80d6b7eae2d94144142e48c0d615a3a0a
Shubham Raut <rautshubham280@gmail.com>
2026-01-29 11:26:33 +0530

    added pdf parseer code

---
+++ app/services/pdf_parser.py
@@ -0,0 +1,23 @@
+from pypdf import PdfReader
+from typing import List, Dict
+import os 
+
+def parsePdf(path : str) -> List[Dict]:
+    reader = PdfReader(path)
+    parsedPages = []
+    
+    filename = os.path.basename(path)
+    
+    for i , page in enumerate(reader.pages):
+        text = page.extract_text()
+        
+        if text and text.strip():
+            parsedPages.append({
+                "page" : i+1,
+                "text" : text.strip(),
+                "source": filename
+            })
+    
+    return parsedPages
+
+    
