11f3e69ee0cb1f7982644c764af60d2cfa37e7d9
Shubham Raut <rautshubham280@gmail.com>
2026-01-29 16:53:38 +0530

    added function for storing the embeddings

--- app/services/vector_store.py
+++ app/services/vector_store.py
@@ -13,3 +13,23 @@ client = Client(Settings(
 ))
 
 collection = client.get_or_create_collection(name=COLLECTION_NAME)
+
+
+
+def store_embeddings(embedded_chunks : List[Dict]):
+    ids = []
+    documents = []
+    metadatas = []
+    embeddings = []
+    
+    for i , chunk in enumerate(embedded_chunks):
+        ids.append(f'chunk_{i}')
+        documents.append(chunk['content'])
+        metadatas.append(chunk['metadata'])
+        embeddings.append(chunk['vector'])
+    
+    collection.add(
+        ids=ids, documents=documents, metadatas=metadatas, embeddings=embeddings
+    )
+    
+    collection.persist()
